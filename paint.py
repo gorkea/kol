f2f374b4eac2d702a0bd910cf615d234bf62ae5d
Can Aydin <ca@fluytsoftware.com>
2015-11-05 02:32:02 +0300

    add attributions and convert to OO

--- paint.py
+++ paint.py
@@ -18,6 +18,9 @@ options like other shapes or colors...
                                                 yours,
                                                 dave mitchell
                                                 davem@magnet.com
+
+Changes to also print out draw commands to stdout by gorke.aygun@gmail.com
+Changes to convert to class by caydinc@gmail.com 
 """
 
 import sys
@@ -25,42 +28,45 @@ from Tkinter import *
 
 """paint.py: not exactly a paint program.. just a smooth line drawing demo."""
 
-b1 = "up"
-xold, yold = None, None
+class BoardCanvas():
+    def __init__(self):
+        self.b1 = "up"
+        self.x = None
+        self.y = None
+        self.root = Tk()
+        self.drawing_area = Canvas(self.root)
+        self.drawing_area.pack()
+        self.drawing_area.bind("<Motion>", self.motion)
+        self.drawing_area.bind("<ButtonPress-1>", self.b1down)
+        self.drawing_area.bind("<ButtonRelease-1>", self.b1up)
 
-def main():
-    root = Tk()
-    drawing_area = Canvas(root)
-    drawing_area.pack()
-    drawing_area.bind("<Motion>", motion)
-    drawing_area.bind("<ButtonPress-1>", b1down)
-    drawing_area.bind("<ButtonRelease-1>", b1up)
-    root.mainloop()
+    def main(self):
+        self.root.mainloop()
 
-def b1down(event):
-    global b1
-    b1 = "down"  
-    print "GOTO %d %d" % (event.x,   event.y)
-    print "ZDOWN"    # you only want to draw when the button is down
+    def b1down(self,event):
+        self.b1 = "down"  
+        print "GOTO %d %d" % (event.x, event.y)
+        print "ZDOWN"    # you only want to draw when the button is down
                           # because "Motion" events happen -all the time-
 
-def b1up(event):
-    global b1, xold, yold
-    b1 = "up"
-    xold = None           # reset the line when you let go of the button
-    yold = None
-    print "ZUP"
-    print ""
-    sys.stdout.flush()
+    def b1up(self,event):
+        self.b1 = "up"
+        self.x = None           # reset the line when you let go of the button
+        self.y = None
+        print "ZUP"
+        print ""
+        sys.stdout.flush()
 
-def motion(event):
-    if b1 == "down":
-        global xold, yold
-        if xold is not None and yold is not None:
-            event.widget.create_line(xold,yold,event.x,event.y,smooth=TRUE)
-            print "MOVE %d %d" % (event.x - xold, event.y - yold)           # here's where you draw it. smooth. neat.
-        xold = event.x
-        yold = event.y
+    def motion(self,event):
+        if self.b1 == "down":
+            if self.x is not None and self.y is not None:
+                event.widget.create_line(self.x,self.y,event.x,event.y,smooth=TRUE)
+                print "MOVE %d %d" % (event.x - self.x, event.y - self.y)
+                # here's where you draw it. smooth. neat.
+        self.x = event.x
+        self.y = event.y
 
 if __name__ == "__main__":
-    main()
+    c = BoardCanvas()
+    c.main()
+
